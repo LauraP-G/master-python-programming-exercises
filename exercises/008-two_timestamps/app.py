8fad308fe044880676de4f0e2be0e060d7170c9e
Laura <151856378+LauraP-G@users.noreply.github.com>
2024-07-19 14:19:03 +0000

    solucionando ejercicios

--- exercises/008-two_timestamps/app.py
+++ exercises/008-two_timestamps/app.py
@@ -1,4 +1,14 @@
 def two_timestamp(hr1, min1, sec1, hr2, min2, sec2):
+    horas_a_segundos_primer=hr1 * 3600
+    horas_a_segundor_segun=hr2 * 3600
+    minutos_a_segundos_primer = min1 * 60
+    minutos_a_segundos_segun = min2 * 60
+    suma_primer= horas_a_segundos_primer+minutos_a_segundos_primer +sec1
+    suma_segun = horas_a_segundor_segun+minutos_a_segundos_segun +sec2
+    return suma_segun - suma_primer  
+
+
+
     # Your code here   
     return None
 
